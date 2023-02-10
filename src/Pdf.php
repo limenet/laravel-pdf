f43eaf47543710f14660a8c2dd3465775056b897
Linus Metzler <hi@linusmetzler.me>
2023-02-10 13:28:32 +0100

    fix font issues

--- src/Pdf.php
+++ src/Pdf.php
@@ -152,6 +152,10 @@ class Pdf
                     'left' => $this->marginLeft,
                 ],
             ],
+            'gotoOptions' => [
+                'waitUntil' => 'networkidle2',
+            ],
+            'userAgent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
         ];
 
         if (app()->environment('local')) {
