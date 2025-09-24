175d7cd035ddd9918ca0198f86e701c870dc8cea
Linus Metzler <hi@linusmetzler.me>
2025-09-24 09:58:50 +0200

    hotfix to disable upstream broken user agent

--- src/Adapters/BrowserlessAdapter.php
+++ src/Adapters/BrowserlessAdapter.php
@@ -41,7 +41,7 @@ class BrowserlessAdapter implements AdapterInterface, ConcurrencyLimiterInterfac
             'gotoOptions' => [
                 'waitUntil' => 'networkidle2',
             ],
-            'userAgent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+            // 'userAgent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
         ];
 
         if (app()->environment('local')) {
